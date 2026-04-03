f5f727cf5a36c823e84024c3ca5dc6deb0f31594
Abduqodirov Sarvar <abduqodirovsarvar.2002@gmail.com>
2026-04-03 16:22:13 +0500

    addd

--- Models/DTOs/PaginationParams.cs
+++ Models/DTOs/PaginationParams.cs
@@ -2,14 +2,19 @@ namespace LoginApp.Models.DTOs;
 
 public class PaginationParams
 {
-    private const int MaxPageSize = 100;
+    private const int MaxPageSize = 500;
     private int _pageSize = 20;
+    private int _pageNumber = 1;
 
-    public int PageNumber { get; set; } = 1;
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set => _pageNumber = value < 1 ? 1 : value;
+    }
 
     public int PageSize
     {
         get => _pageSize;
-        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        set => _pageSize = value < 1 ? 1 : value > MaxPageSize ? MaxPageSize : value;
     }
 }
