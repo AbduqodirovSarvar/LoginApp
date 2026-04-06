aabf38cbe71f1ac620b6761a3da19758c03ca540
Abduqodirov Sarvar <abduqodirovsarvar.2002@gmail.com>
2026-04-06 15:52:07 +0500

    add one role

--- DB/Enums/UserRole.cs
+++ DB/Enums/UserRole.cs
@@ -2,7 +2,8 @@ namespace LoginApp.DB.Enums;
 
 public enum UserRole
 {
-    User = 0,
-    Admin = 1,
-    Pro_User = 2,
+            User = 0,
+            Admin = 1,
+            Pro_User = 2,
+            Vip_User = 3
 }
